f83971909f437af7f8c9c8b8b68d90e570c44c11
Carsten Koch <carstenkoch@Carstens-MacBook-Pro.local>
2018-06-02 10:31:35 +0200

    Project Settings were not loaded

--- src/config/load-project-settings.js
+++ src/config/load-project-settings.js
@@ -11,6 +11,8 @@
     }
 
     sbVideoScript.loadProjectSettings = function () {
+        var MAX_PARENTS = 2;
+
         function updateSetting(global, local) {
             for (var key in local) {
                 if (key === 'toJSON') {
@@ -24,24 +26,43 @@
             return global;
         }
 
-        function adaptSettings(parentSettings, childSettings) {
-            sbVideoScript.settings = updateSetting(sbVideoScript.settings, parentSettings);
-            return childSettings;
+        function adaptSettings(projSettingsArr) {
+            for (var i = projSettingsArr.length; i > 0; i--) {
+                var settings = projSettingsArr[i-1];
+                sbVideoScript.settings = updateSetting(sbVideoScript.settings, settings);
+            }
         }
 
-        function loadSettings(folder, iteration, childSettings) {
-            var MAX_PARENTS = 2;
-            if (iteration === MAX_PARENTS) return childSettings;
+        // function adaptSettings(parentSettings, childSettings) {
+        //     sbVideoScript.settings = updateSetting(sbVideoScript.settings, parentSettings);
+        //     return childSettings;
+        // }
+
+        function loadSettings(folder, iteration, projSettingsArr) {
+            if (iteration === MAX_PARENTS) return;
             var settings = {};
             try {
-                settings = JSON.parse(loadFile(folder.fsName + '/project-settings.json'));
+                settings = JSON.parse(loadFile(folder.fsName + 'project-settings.json'));
+                projSettingsArr.push(settings);
             } catch (e) {
             }
-            return adaptSettings(loadSettings(folder.parent, iteration + 1, settings), childSettings);
+            loadSettings(folder.parent, iteration + 1, projSettingsArr);
         }
 
+        // function loadSettings(folder, iteration, childSettings) {
+        //     if (iteration === MAX_PARENTS) return childSettings;
+        //     var settings = {};
+        //     try {
+        //         settings = JSON.parse(loadFile(folder.fsName + '/project-settings.json'));
+        //     } catch (e) {
+        //     }
+        //     return adaptSettings(loadSettings(folder.parent, iteration + 1, settings), settings);
+        // }
+
         try {
-            loadSettings(app.project.file.parent, 0, {});
+            var projSettingsArr = [];
+            loadSettings(app.project.file.parent, 0, projSettingsArr);
+            adaptSettings(projSettingsArr);
 
         } catch (e) {
             throw new sbVideoScript.RuntimeError({
